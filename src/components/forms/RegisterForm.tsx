ed4cb307c8ac6a23e23c1972f9d483b67a00c13e
mayank <mayankgupta14042000@gmail.com>
2023-02-04 01:02:22 +0530

    new components

---
+++ src/components/forms/RegisterForm.tsx
@@ -0,0 +1,8 @@
+
+export default function RegisterForm() {
+  return (
+    <form>
+
+    </form>
+  )
+}
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,9 @@
+
+import { NextPage } from 'next'
+import styles from './index.module.scss'
+
+const RegisterPage: NextPage = () => {
+    return <div className={styles.page}>Register</div>
+}
+
+export default RegisterPage
